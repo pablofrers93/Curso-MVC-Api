eb2fd9b337bdae6e098c1c10c0be67f2ed72d4d6
pablofrers93 <pablofrers93@gmail.com>
2023-04-14 21:56:45 -0300

    creating repo

---
+++ CursoMVC_Api/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CursoMVC_Api.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ CursoMVC_Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CursoMVC_Api.WebApiApplication" Language="C#" %>
